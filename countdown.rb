c9acfe54393df2c0a2afcb8c93dc1ad2284e4f01
Danny Brandt <dvbrandt90@gmail.com>
2020-12-04 15:17:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,11 +8,11 @@ def countdown(num)
  "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep (num, pause)
+def countdown_with_sleep (num, 1)
    while num > 0
   puts "#{num} SECOND(S)!"
   num -= 1 
-  sleep(pause)
+  sleep(1)
  end
  "HAPPY NEW YEAR!"
 end
